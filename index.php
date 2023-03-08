cc559332d370fc2996901a0e68006193e973cb20
Ilya-Neverov <102865207+Ilya-Neverov@users.noreply.github.com>
2023-03-08 20:33:33 +0300

    v1
    первая верстка, в первый раз использовал псевдо класс css

---
+++ index.php
@@ -0,0 +1,155 @@
+<!doctype html>
+<html lang="ru">
+    <head>
+        <meta charset="utf-8">
+        <link rel="icon" href="image/icon_camera.png">
+        <link href="styles.css" rel="stylesheet" type="text/css"/>
+        <title>Denis Novik</title>
+    </head>
+    <body>
+
+        <nav>
+            <ol>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about_me">About me</a></li>
+                <li><a href="#skills">Skills</a></li>
+                <li><a href="#portfolio">Porttfolio</a></li>
+                <li><a href="#contacts">Contacts</a></li>
+            </ol>
+            <hr>
+        </nav>
+
+        <a name="home"></a>
+        <header>
+            <div class="name">
+                <p>Denis</p>
+                <p>Novik</p>
+            </div>
+            <div class="annotation">
+                <p>UX | UI designer </p>
+                <p>24 years old, Minsk</p>
+            </div>
+        </header>
+        <img class="foto" src="image/denis_novik.png" alt=""/>
+
+        <section class="about_me">
+            <a name="about_me"></a>  
+            <h2>About me</h2>
+            <div class="about_me_text">
+                <p>Hi, I'm Denis – UX/UI designer from Minsk.</p>
+                <p>I'm interested in design and everything connected with it.</p>
+            </div>
+            <div class="about_me_text">
+                <p> I'm studying at courses "Web and mobile design </p>
+                <p> interfaces" in IT-Academy.</p>
+            </div>
+            <div class="about_me_text"> 
+                <p>  Ready to implement excellent projects</p>
+                <p>  with wonderful people.</p>
+            </div>
+        </section>
+
+        <section class="skills">
+            <a name="skills"></a> 
+            <h2>Skills</h2>
+            <p class="Skills_text">I work in such programs as</p>
+
+            <div class="skills_logo">
+                <figure class="skils_figure">
+                    <img class="skills_img" src="image/PS.png" alt=""/>
+                    <p class="skills_tech">Adobe <br> Photoshop</p>
+                    <div class="asterisks">
+                        <span class style="color: #010101">&#9733;</span>
+                        <span class style="color: #010101">&#9733;</span>
+                        <span class style="color: #010101">&#9733;</span>
+                        <span class style="color: #010101">&#9733;</span>
+                        <span style="color: #e5e5e5">&#9733;</span>
+                    </div>
+                </figure>
+
+                <figure class="skils_figure">
+                    <img class="skills_img" src="image/AI.png" alt=""/>
+                    <p class="skills_tech">Adobe <br> Illustrator</p>
+                    <div class="asterisks">
+                        <span class style="color: #010101">&#9733;</span>
+                        <span class style="color: #010101">&#9733;</span>
+                        <span class style="color: #010101">&#9733;</span>
+                        <span style="color: #e5e5e5">&#9733;</span>
+                        <span style="color: #e5e5e5">&#9733;</span>
+                    </div>
+                </figure>
+
+                <figure class="skils_figure">
+                    <img class="skills_img" src="image/AE.png" alt=""/>
+                    <p class="skills_tech">Adobe <br> After Effects</p>
+                    <div class="asterisks">
+                        <span class style="color: #010101">&#9733;</span>
+                        <span class style="color: #010101">&#9733;</span>
+                        <span class style="color: #010101">&#9733;</span>
+                        <span class style="color: #010101">&#9733;</span>
+                        <span style="color: #e5e5e5">&#9733;</span>
+                    </div>
+                </figure>
+
+                <figure class="skils_figure">
+                    <img class="skills_img" src="image/Figma.png" alt=""/>
+                    <p class="skills_tech">Adobe <br> After Effects</p>
+                    <div class="asterisks">
+                        <span class style="color: #010101">&#9733;</span>
+                        <span class style="color: #010101">&#9733;</span>
+                        <span class style="color: #010101">&#9733;</span>
+                        <span class style="color: #010101">&#9733;</span>
+                        <span style="color: #e5e5e5">&#9733;</span>
+                    </div>
+                </figure>
+            </div>
+        </section>
+
+
+        <section class="portfolio">
+            <a name="portfolio"></a>   
+            <h2 class="portfolio_heder">Portfolio</h2>
+            <figure class="portfolio_card">
+                <img class="foto" src="image/online-fashion.png" alt=""/>
+                <a class="portfolio_a" href="https://www.online-fashion.ru/" target="_blank">Online fashion store - Homepage</a>
+            </figure>
+
+            <figure class="portfolio_card">
+                <img class="foto" src="image/reebok_web.png" alt=""/>
+                <a class="portfolio_a" href="https://stores.reebok.com/" target="_blank">Reebok Store - Concept</a>
+            </figure>
+
+            <figure class="portfolio_card">
+                <img class="foto" src="image/braun.png" alt=""/>
+                <a class="portfolio_a" href="https://braun-russia.ru/" target="_blank">Braun Landing Page - Concept</a>
+            </figure>
+        </section>
+
+
+        <section class="contacts">
+            <a name="contacts"></a>
+            <h2 class="contacts_h2">Contacts</h2>
+            <div class="contacts_text">
+                <p>Want to know more or just chat?</p>
+                <p>You are welcome!</p>
+            </div>
+
+            <div class="contacts_mail">
+                <p class="contacts_mail_text">mailto:vlad@htmlbook.ru</p>
+            </div>
+
+            <div class="contacts_logo">
+                <a href="https://linkedin.com/" target="_blank"><img src="image/logo_LinkedIn.png" alt=""/></a>
+                <a href="https://instagram.com/" target="_blank"><img src="image/logo_Instagram.png" alt=""/></a>
+                <a href="https://behance.net/" target="_blank"><img src="image/logo_Behance.png" alt=""/></a>
+                <a href="https://dribbble.com/" target="_blank"><img src="image/logo_dribbble.png" alt=""/></a>
+            </div>
+
+            <div class="contacts_text_2">     
+                <p>Like me on</p>
+                <p>LinkedIn, Instagram, Behance, Dribble</p>
+            </div>       
+
+        </section>
+    </body>
+</html>
